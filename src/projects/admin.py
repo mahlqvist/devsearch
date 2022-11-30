cb15f9648ba8c51b74f642ce1500aa176202b3cc
mahlqvist <mahlqvist@msn.com>
2022-11-30 09:31:08 +0100

    some changes

--- src/projects/admin.py
+++ src/projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Tag, Project, Review
+
+admin.site.register(Project)
+admin.site.register(Tag)
+admin.site.register(Review)
--- src/projects/views.py
+++ src/projects/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from datetime import datetime
+
+today = datetime.now()
+time = today.strftime('%H:%M:%S')
+list_of_projects = [
+    {'id':'1', 'title': 'E-commerce Website', 'description': 'Fully functional ecommerce website'},
+    {'id':'2', 'title': 'Portfolio Website', 'description': 'My portfolio website of awesomeness'},
+    {'id':'3', 'title': 'Social Network', 'description': 'Awesome project I am still working on'},
+]
 
 def projects(request):
-    return HttpResponse("<h1>Hello world</h1>\n<h3>Here are my projects</h3>")
-
+    return render(request, 'projects/projects.html', {
+        'time': time,
+        'projects': list_of_projects,
+    })
 def project(request, pk):
-    return HttpResponse("<h1>Single project " + str(pk) + "</h1>")
+    project_obj = None
+    for item in list_of_projects:
+        if item.get("id", None) == pk:
+            project_obj = item
+    return render(request, 'projects/single-project.html', {
+        'project': project_obj,
+    })
